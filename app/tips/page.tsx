bbfa97927958692092a823518d129a82dd48bdcd
nishimura lab PC 2 <nishimuratk@keio.jp>
2024-09-11 15:31:16 +0900

    commitしそびれの追加

--- app/tips/page.tsx
+++ app/tips/page.tsx
@@ -1,49 +1,278 @@
-import { GassConlo } from "@/public";
-import Image from "next/image";
+"use client";
 
+import Image, { StaticImageData } from "next/image";
+import { Carrot, CarrotGray, Delete, Folk, FolkGray, Heart, KatsuCurry, Refrigerator, RefrigeratorGray, Save, Soba, TacoRice, TakikomiImage} from "@/public/";
+import { ProfileImage} from "@/public/";
+import { Pasuta} from "@/public";
+import { Cookie} from "@/public";
+import { useEffect, useState } from "react";
+import useApi from "@/hooks/useApi";
 
-const image = <Image className="w-[90vw] mx-auto" src={GassConlo} alt="ガスコンロ" />;
-const title = (text:string) => <h1 className="text-lg font-bold mb-4 ">{text}</h1>;
-const baseText = (text:string) => <div className="mb-4 text-left text-sm">{text}</div>;
-const heading = (text:string) => <h1 className="text-1xl mb-4 relative inline-block text-base">{text}<span className="absolute left-0 bottom-0 w-1/4 h-1 bg-red-500"></span></h1>;
-const middleHeading = (text:string) => <div className="mb-4s text-left text-sm font-bold">{text}</div>;
+export default function Recipe() {
+    // 本文，見出しを設定できる関数
+    const baseText = (text:string) => <div className="mb-4s text-left text-sm">{text}</div>;
+    const middleHeading = (text:string) => <div className="mb-4s text-left text-sm font-bold">{text}</div>;
+    // 材料の省略表記を出力する関数
 
-const context = <div className = "m-10 mx-left">
-    {title("【ガスが止まった時に】火を使わない調理法／節ガス・節水になるポリ袋調理法")}
-    {baseText("地震や大型台風などの災害によって「ガス」が使えなくなったときにできる調理の工夫と、節ガス・節水になる「ポリ袋調理」について解説します。")}
-    
-    {heading("ガスが使えないときにできる調理の工夫")}
-    {middleHeading("カセットコンロを使う")}
-    {baseText("ガスが使えないときの代わりの熱源として、あると役立つのがカセットコンロです。ただし、ガスボンベの燃焼時間には限りがあるので、 加熱するものに優先順位をつけて、効率的に使うよう心がけましょう。コンロを2台並べたり、ストーブのそばで使用したりするのは危険なので避けてください。「カセットコンロの正しい使い方」も確認してください。")}
-    {middleHeading("乾物や缶詰にアレンジを加える")}
-    {baseText("レトルト食品や缶詰はそのまま食べられるのがいいところですが、毎食、加工品ばかりだと飽きがちです。そんなときはひと手間加えて、味つけをアレンジしてみましょう。ガスや電気を使わなくても、乾物を調味料とあえたり、食材を缶詰にのせたりするだけで、立派なおかずになります。手づくりの料理があると、減退しがちな食欲も回復します。")}
-    {middleHeading("電気が使える場合の熱源")}
-    {baseText("災害時に必ずしもすべてのライフラインが止まるわけではありません。電気が使える環境であれば、ガスの代わりに 電子レンジや炊飯器、ホットプレートなどを活用し、普段と変わらない食事を楽しめます。")}
-
-    {heading("制限のある環境下で役立つ「ポリ袋調理」")}
-    {baseText("ポリ袋調理は、災害時の制限ある環境下で役立つ調理法です。基本は食材を入れて、湯せんにかけるだけですが、 いくつかポイントがあります。")}
-    {middleHeading("1. ポリ袋に食材と調味料を入れる")}
-    {baseText("深めの器などにポリ袋をかぶせてから食材を入れます。こぼれる心配がなく、特に水などの液体を加えるときは安心です。もちろんポリ袋にそのまま食材を入れても問題ありません。")}
-    {middleHeading("2. 手でもんで混ぜる")}
-    {baseText("あえたり、味をなじませたり、こねたりといった作業をポリ袋をもんで行います。ボウルや泡立て器などの調理器具がなくても大丈夫です。")}
-    {middleHeading("3. 空気を抜いて上の方で口を結ぶ")}
-    {baseText("加熱すると空気が膨張し、破裂する可能性があるので、空気が入らないようねじり上げ、 袋の上部でかたく結びます（結んだあとは、 ねじれが戻ってもOK）。")}
-    {middleHeading("4. 鍋に水をはり、耐熱皿を敷く")}
-    {baseText("ポリ袋が鍋底に直接当たると、熱で破れる可能性があるので、水を入れて耐熱皿を敷いておきます。")}
-    {middleHeading("5. 鍋にポリ袋を入れて加熱する")}
-    {baseText("食材の入ったポリ袋を、水の状態から入れます。鍋に湯を沸かしてから入れてもいいですが、水からの方がエネルギーを効率的に使うことができます。加熱中はなるべくフタをして、蒸発を防いでください。")}
-    {middleHeading("6. トングなどで取り出して、器に盛る")}
-    {baseText("火がとおったら、トングや菜箸で袋をすくい上げます。結び目の下をハサミで切り、器に移し替えるか、もしくはそのままポリ袋を器にかぶせて食卓に出しましょう。")}
-</div>;
-
-const Tips = () => {
-    return (
-        <div>
-            {image}
-            {context}
-        </div>
+    function materialsText(materials: string[]){
+        // 表示する材料の最大個数，これを上回るものは"…"で省略される
+        const maxMaterials = 3;
+        if(materials.length <= maxMaterials){
+            return materials.join(", ")
+        }else{
+            return materials.slice(0,maxMaterials).join(", ") + " …"
+        }
+    }
+    // アイコン画像を作成する関数
+    const iconImage = (src: StaticImageData, text: string, width: number = 20, height: number = 20) => (
+        <Image
+            src={src}
+            alt={text}
+            width={width}
+            height={height}
+            className="object-contain"  // 画像の縦横比を維持しつつ、指定サイズ内に収める
+        />
     );
-};
 
+    // レシピを表すカードの描写に関する部分
+    // デモデータ
 
-export default Tips;
+    const { data, error, loading, refetch } = useApi();
+    useEffect(() => {
+        refetch('/recipes', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+        });
+        console.log("test");
+    }, [refetch])
+    useEffect(() => {
+        console.log(data);
+    }, [data])
+    
+    const recipes = [
+        {
+            recipe_id: 0,
+            title: "炊き込みご飯",
+            image: TakikomiImage,
+            materials: ["米", "キノコ", "醤油"],
+            addedDate: new Date("2024-09-11T10:00:00Z"),
+            savedDate: new Date("2025-09-11T10:00:00Z"),
+            rating: 20,
+            creationTime: 40,
+            obtainMaterials: 3,
+            life: 2,
+            taste: 4
+        },
+        {
+        recipe_id: 1,
+        title: "和風キノコパスタ",
+        image: Pasuta,
+        materials: ["乾麺", "キノコ", "醤油", "バター", "塩"],
+        addedDate: new Date("2024-09-13T10:00:00Z"),
+        savedDate: new Date("2026-09-11T10:00:00Z"),
+        rating: 30,
+        creationTime: 15,
+        obtainMaterials: 2,
+        life: 5,
+        taste: 1
+        },
+        {
+        recipe_id: 2,
+        title: "手作りクッキー",
+        image: Cookie,
+        materials: ["卵", "砂糖", "小麦粉", "バター", "塩"],
+        addedDate: new Date("2024-08-10T10:00:00Z"),
+        savedDate: new Date("2027-09-11T10:00:00Z"),
+        rating: 78,
+        creationTime: 40,
+        obtainMaterials: 2,
+        life: 1,
+        taste: 3
+        },
+        {
+        recipe_id: 3,
+        title: "とろろそば",
+        image: Soba,
+        materials: ["乾麺", "ねぎ", "山芋", "めんつゆ", "ワサビ"],
+        addedDate: new Date("2024-02-10T10:00:00Z"),
+        savedDate: new Date("2028-09-11T10:00:00Z"),
+        rating: 2,
+        creationTime: 12,
+        obtainMaterials: 4,
+        life: 4,
+        taste: 1
+        },
+        {
+        recipe_id: 4,
+        title: "カツカレー",
+        image: KatsuCurry,
+        materials: ["レトルトカレー", "米", "ひれ肉", "卵", "片栗粉", "パン粉", "野菜（お好み）"],
+        addedDate: new Date("2024-05-10T10:00:00Z"),
+        savedDate: new Date("2025-11-11T10:00:00Z"),
+        rating: 201,
+        creationTime: 20,
+        obtainMaterials: 5,
+        life: 2,
+        taste: 3
+        },
+        {
+        recipe_id: 5,
+        title: "タコライス",
+        image: TacoRice,
+        materials: ["米", "カレー粉", "キャベツ", "チーズ", "タバスコ"],
+        addedDate: new Date("2023-12-10T10:00:00Z"),
+        savedDate: new Date("2025-02-11T10:00:00Z"),
+        rating: 22,
+        creationTime: 10,
+        obtainMaterials: 3,
+        life: 4,
+        taste: 1
+
+        },
+    ];
+    const [recipeList, setresipeList] = useState([...recipes]);
+    const handleRecipes = (recipes) => {
+        setSelected(recipes);
+    }
+    // 評価値を表すアイコンのmap
+    const iconMap = [
+        {key: "obtainMaterials", colorIcon: Carrot, grayIcon: CarrotGray, text:"素材の調達は簡単でしたか"},
+        {key: "life", colorIcon: Refrigerator, grayIcon: RefrigeratorGray, text:"長期保存はしやすいと感じますか"},
+        {key: "taste", colorIcon: Folk, grayIcon: FolkGray, text: "もう一度食べたいと思いますか"},
+    ];
+    function rateIcons(rate: number, ColordIcon: StaticImageData, GrayIcon: StaticImageData, text: string) {
+        // 評価の最大値の5まで繰り返す
+        const iconList = Array.from({ length: 5 }, (_, i) => i < rate ? ColordIcon : GrayIcon);
+    
+        return (
+            <div className="mt-1 mb-1">
+                <ul className="flex list-none p-0 m-0">
+                    {iconList.map((item, index) => (
+                        <li key={index} className="mr-1">
+                            {iconImage(item, text)}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+    // デモデータを受けてカードを作成する関数
+    function recipeCard(recipeMap: {
+        [x: string]: any;
+        recipe_id?: number;
+        title: string;
+        image: StaticImageData;
+        materials: string[];
+        addedDate: Date;
+        rating?: number;
+        creationTime?: number;
+    }) {
+        return (
+            <div className="w-[90vw] mx-auto my-5 flex">
+                <div className="flex-1 bg-gray-300 rounded-lg p-5 flex">
+                    <div className="w-2/3 text-left pr-4">
+                        {middleHeading(recipeMap.title)}
+                        {baseText(`材料 ：${materialsText(recipeMap.materials)}`)}
+                        {baseText(`作成時間 ：${recipeMap.creationTime}分`)}
+                        <ul className="list-none p-0">
+                            {iconMap.map((iconMap, index) => (
+                                <li key={index} className="inline-block mr-2">
+                                    {rateIcons(recipeMap[iconMap.key], iconMap.colorIcon, iconMap.grayIcon, iconMap.text)}
+                                </li>
+                            ))}
+                        </ul>
+                        {baseText(`投稿日時 ：${recipeMap.addedDate.toLocaleDateString('ja-JP', {
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit',
+                        })}`)}
+                        {baseText(`保存日時 ：${recipeMap.savedDate.toLocaleDateString('ja-JP', {
+                            year: 'numeric',
+                            month: '2-digit',
+                            day: '2-digit',
+                        })}`)}
+                    </div>
+    
+                    <div className="w-1/3 flex flex-col justify-between">
+                    <div className="flex flex-col items-center">
+                        <Image className="w-full h-auto rounded-md" src={recipeMap.image} alt="料理画像" />
+                    </div>
+                    <div className="flex items-center mt-3">
+                        {iconImage(Heart, "ハートのアイコン")}
+                        <span className="text-lg font-semibold ml-2">{recipeMap.rating}</span>
+                    </div>
+                </div>
+            </div>
+    
+                <div className="min-w-[30px] flex flex-col items-center justify-start px-3">
+                    <div className="flex-none">
+                        {iconImage(Delete, "保存")}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    
+    // ソート切り替えボタンを追跡するハンドラ
+    const [selected, setSelected] = useState(-1);
+    const handleClick = (buttonId) => {
+        setSelected(buttonId);
+    }
+    // ボタンのひな型
+    const buttonTable = [
+        {id: 0, name: "新着順", onClick: ()=>{sortByAddedDate(0)}},
+        {id: 1, name: "人気度順", onClick: ()=>{sortByRate(1)}},
+        {id: 2, name: "作成時間順", onClick: ()=>{sortByCreationTime(2)}},
+    ]
+    // 各種ソート関数
+    function sortListByFunction(list: any[], func: { (user: any): any; (user: any): any; (arg0: any): any; }) {
+        return list.sort((a, b) => {
+            const valueA = func(a);
+            const valueB = func(b);
+            
+            if (valueA < valueB) return -1;
+            if (valueA > valueB) return 1;
+            return 0;
+        });
+    }
+    function sortByAddedDate(id){
+        const newRecipe = sortListByFunction(recipeList, ((recipe)=>-recipe.addedDate.getTime()))
+        setresipeList(newRecipe);
+        handleClick(id);
+    }
+    function sortByRate(id){
+        const newRecipe = sortListByFunction(recipeList, ((recipe)=>-recipe.rating))
+        setresipeList(newRecipe);
+        handleClick(id);
+    }
+    function sortByCreationTime(id){
+        const newRecipe = sortListByFunction(recipeList, ((recipe)=>recipe.creationTime))
+        setresipeList(newRecipe);
+        handleClick(id);
+    }
+    // 選択されているかを受けてボタンのデザインを変化させる関数
+    function sortButton(button, selectedId){
+        return (<button className={`inline-flex items-center px-4 border ${button.id === selectedId ? "bg-blue-500 text-white" : 'bg-gray-200 text-gray-700'}`} onClick={button.onClick}>
+            {button.name}
+        </button>);
+    }
+
+    return (
+        <div className="m-5">
+            <div className="flex w-[80vw] max-w-md mx-auto">
+                {buttonTable.map((button)=>sortButton(button, selected))}
+            </div>
+
+            <ul>
+                {recipeList.map((recipe, index) => (
+                <li key={index}>
+                    {recipeCard(recipe)}
+                </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
