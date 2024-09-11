84d99ff769d2ec2c285168f6025a1d6d5893781c
nishimura lab PC 2 <nishimuratk@keio.jp>
2024-09-11 10:42:26 +0900

    feature/rak-37 public無い画像共有のために一旦push

--- public/index.ts
+++ public/index.ts
@@ -7,6 +7,13 @@ export { default as KatsuCurry } from './curry.jpg'
 export { default as Soba } from './soba.jpg'
 export { default as Cookie } from './cookie.jpg'
 export { default as Pasuta } from './pasuta.jpg'
+export { default as Carrot } from './carrot_black.png'
+export { default as CarrotGray } from './carrot_139.png'
+export { default as Folk } from './folk_black.png'
+export { default as FolkGray } from './folk_139.png'
+export { default as Refrigerator } from './freeze_black.png'
+export { default as RefrigeratorGray } from './freeze_139.png'
+export { default as Heart } from './heart.png'
 
 
 
