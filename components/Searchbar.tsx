2f6c17e23b2f802a3eeacb2e6ff61692de19398e
satocchi0416sh <satoyoshi44@gmail.com>
2024-09-12 17:29:24 +0900

    feat: searchバーを直した

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { useRouter } from 'next/navigation'
 import { useSearchContext } from '@/feature/recipes'
 
 const Searchbar: React.FC = () => {
+
+    const [isFocused, setIsFocused] = useState(false);
     const router = useRouter()
     const { setSearchQuery } = useSearchContext()
 
@@ -27,16 +29,31 @@ const Searchbar: React.FC = () => {
     }
 
     return (
-        <form onSubmit={handleSubmit} className="relative w-full">
-            <input
-                type="text"
-                name="search"
-                placeholder="材料、道具、料理名で検索"
-                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-                onKeyPress={handleKeyPress}
-            />
-            <BiSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl" />
-        </form>
+        <label htmlFor="search" className="relative w-full">
+            <form onSubmit={handleSubmit} >
+                <input
+                    type="text"
+                    name="search"
+                    placeholder="材料、道具、料理名で検索"
+                    className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                    onKeyPress={handleKeyPress}
+                />
+                <BiSearch className={`absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 text-xl ${isFocused ? 'text-blue-500' : ''}`} />
+                {
+                    isFocused && (
+                        <div className="left-0 top-0 w-full h-full bg-white" >
+                            <h1>絞り込み</h1>
+                            <div>
+                                <h2>材料</h2>
+                                <div>
+                                    <input type="text" placeholder="材料を入力" />
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </form>
+        </label>
     )
 }
 
